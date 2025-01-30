4a060cedde528fed03fca8ff89f1ee748d3c3f26
Josip Prpić <josippr@gmail.com>
2025-01-30 21:36:04 +0100

    initial commit

---
+++ task1.py
@@ -0,0 +1,11 @@
+# SOJF Seminarski
+# Author: Josip Prpić
+# Date: 30-01-2025
+# Github repo: https://github.com/josippr/TVZ-SOJF-final
+
+def main():
+  print("Hello World")
+
+
+if __name__ == "__main__":
+  main()
